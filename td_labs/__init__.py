432b0d9fa14a5cae25bc9c1a63159e47621da7e5
Leslie <leslie.kep@gmail.com>
2018-11-13 14:24:45 +0200

    update init

---
+++ td_labs/__init__.py
@@ -0,0 +1,5 @@
+from .infant_panels import infant_glucose_panel, infant_insulin, dna_pcr, serum_panel
+from .infant_panels import infant_pbmc_pl_panel, dbs_panel
+
+from .maternal_panels import cd4_panel, viral_load, pbmc_vl_panel, fasting_glucose
+from .maternal_panels import glucose_1h, glucose_2h, pbmc_pl_panel, elisa_panel, insulin
