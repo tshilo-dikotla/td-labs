363469177b0bc5aa9caa47cde6d82daf8d148dec
Leslie <leslie.kep@gmail.com>
2018-11-13 10:38:07 +0200

    add derivatives

--- td_labs/aliquot_type.py
+++ td_labs/aliquot_type.py
@@ -9,3 +9,5 @@ serum = AliquotType(name='Serum', alpha_code='SERUM', numeric_code='06')
 wb = AliquotType(name='Whole Blood', alpha_code='WB', numeric_code='02')
 
 pbmc = AliquotType(name='PBMC', alpha_code='PBMC', numeric_code='31')
+
+wb.add_derivatives(bc, pl, serum, pbmc, wb)
