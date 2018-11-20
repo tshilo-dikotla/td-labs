6e643fe1e2dfede74f7ac19a1295260327681137
Leslie <leslie.kep@gmail.com>
2018-11-20 17:24:27 +0200

    delete labs.py

--- td_labs/labs.py
+++
@@ -1,6 +0,0 @@
-from edc_lab.site_labs import site_labs
-
-# from .infant_lab_profiles import infant_lab_profile
-from .maternal_lab_profiles import maternal_lab_profile
-
-site_labs.register(maternal_lab_profile)
