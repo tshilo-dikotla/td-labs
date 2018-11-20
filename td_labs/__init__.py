610f0e04b7ab47a508e28510b1654f92e5f4df48
Coulson Kgathi <ckgathi@gmail.com>
2018-11-20 17:00:25 +0200

    disable requisitions

--- td_labs/__init__.py
+++ td_labs/__init__.py
@@ -1,6 +1,6 @@
 from .infant_panels import infant_glucose_panel, infant_insulin, dna_pcr, serum_panel
 from .infant_panels import infant_pbmc_pl_panel, dbs_panel
 
-from .maternal_panels import cd4_panel, viral_load_panel, pbmc_vl_panel
+from .maternal_panels import viral_load_panel, pbmc_vl_panel, cd4_panel
 from .maternal_panels import fasting_glucose_panel, glucose_1h_panel, glucose_2h_panel
 from .maternal_panels import pbmc_pl_panel, elisa_panel, insulin_panel
